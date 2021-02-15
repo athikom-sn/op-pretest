1dc1fa66187ed10658b1d69f3fc8114152bf09fa
JSC <73455330+athikom-sn@users.noreply.github.com>
2021-02-15 22:54:22 +0700

    Athikom : Upload Project 02/15/2021
    V.1.0.0
    Upload simple resource.

---
+++ pretest/assets/color/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kEmptyColor = 0x0D000000;
+
+// Main Color
+const mainColor = Color(0xffaba0ca);
+const mainColorSplash = Color(0xffAA9FCB);
+const mainColorBG = Color(0xffE9E8F5);
